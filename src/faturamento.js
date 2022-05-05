d1524739e5f7be0fc1e4f0a560b37365b554b8c0
Eduardo Ribeiro <eduardoribeiro17@gmail.com>
2022-05-04 22:45:48 -0300

    Atualizações

---
+++ src/faturamento.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const csv = require('csvtojson/v2')
+const { writeFile } = require('fs')
+
+const dataFile =
+  'C:/Users/eduardo/Documents/dev/lab/parse-layout-protheus/data-file/03-2022-FY.csv'
+const file = 'C:/ct2/032022-FY.txt'
+
+module.exports = (async function parse() {
+  const rows = await csv({
+    output: 'json',
+    delimiter: ';',
+  }).fromFile(dataFile)
+
+  let i = 1
+  for (const row of rows) {
+    let {
+      linha,
+      conta_debito,
+      conta_credito,
+      valor,
+      historico,
+      cc_debito,
+      cc_credito,
+    } = row
+
+    linha = '003'
+
+    conta_debito = fill(conta_debito, 8)
+    conta_credito = fill(conta_credito, 8)
+    valor = valor.replace(/[^\d]+/g, '')
+    valor = fill(valor, 8)
+    historico = fill(historico, 20)
+    cc_debito = fill(cc_debito, 12)
+    cc_credito = fill(cc_credito, 12)
+
+    const line = `${linha} ${conta_debito} ${conta_credito} ${valor} ${historico} ${cc_debito} ${cc_credito}\r\n`
+    // 79 + 1 considerando caracter de final de linha
+    if (line.length > 79)
+      console.warn(
+        line,
+        `-> Linha ${i}: ${line.length} 
+        | CT Debito: ${conta_debito.length} 
+        | CT Crédito: ${conta_credito.length} 
+        | Valor: ${valor.length} 
+        | Historico: ${historico.length} 
+        | CC Debito: ${cc_debito.length} 
+        | CC Credito: ${cc_credito.length}`
+      )
+
+    writeFile(file, line, { enconding: 'utf-8', flag: 'a' }, err => {
+      if (err) throw err
+    })
+
+    i++
+  }
+
+  console.log(`FILE PARSE FINISHED! Lines: ${i}`)
+})()
+
+function fill(value, totalLength) {
+  const valueLength = value.length
+  const diff = totalLength - value.length
+
+  if (valueLength < totalLength) {
+    const blankSpace = ' '.repeat(diff)
+
+    return `${value}${blankSpace}`
+  }
+
+  return value
+}
--- src/folha.js
+++ src/folha.js
@@ -3,15 +3,19 @@
 const csv = require('csvtojson/v2')
 const { writeFile } = require('fs')
 
-const dataFile = 'data-file/01-2020.csv'
-const file = 'export/01-2020-test.txt'
+const dataFile =
+  'C:/Users/eduardo/Documents/dev/lab/parse-layout-protheus/data-file/03-2022.csv'
+const file = 'C:/ct2/032022.txt'
 
 module.exports = (async function parse() {
+  // const last_cc_credito = ''
+
   const rows = await csv({
     output: 'json',
     delimiter: ';',
   }).fromFile(dataFile)
 
+  let i = 1
   for (const row of rows) {
     let {
       linha,
@@ -23,24 +27,37 @@ module.exports = (async function parse() {
       cc_credito,
     } = row
 
-    linha = linha.substr(3, 6)
+    linha = '003'
 
     conta_debito = fill(conta_debito, 8)
     conta_credito = fill(conta_credito, 8)
     valor = valor.replace(/[^\d]+/g, '')
     valor = fill(valor, 8)
-    historico = fill(historico, 19)
-    cc_debito = fill(cc_debito, 10)
-    cc_credito = fill(cc_credito, 10)
+    historico = fill(historico, 20)
+    cc_debito = fill(cc_debito, 12)
+    cc_credito = fill(cc_credito, 12)
 
-    const line = `${linha} ${conta_debito} ${conta_credito} ${valor} ${historico} ${cc_debito} ${cc_credito}\n`
-
-    console.warn(line, line.length)
+    const line = `${linha} ${conta_debito} ${conta_credito} ${valor} ${historico} ${cc_debito} ${cc_credito}\r\n`
+    if (line.length > 79)
+      console.warn(
+        line,
+        `-> Linha ${i}: ${line.length} 
+        | CT Debito: ${conta_debito.length} 
+        | CT Crédito: ${conta_credito.length} 
+        | Valor: ${valor.length} 
+        | Historico: ${historico.length} 
+        | CC Debito: ${cc_debito.length} 
+        | CC Credito: ${cc_credito.length}`
+      )
 
     writeFile(file, line, { enconding: 'utf-8', flag: 'a' }, err => {
       if (err) throw err
     })
+
+    i++
   }
+
+  console.log(`FILE PARSE FINISHED! Lines: ${i}`)
 })()
 
 function fill(value, totalLength) {
